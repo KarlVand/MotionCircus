8c873c5d11b906a7dd0ab83e53894129b38dd0ab
KarlVand <cg.vandooren.karl@gmail.com>
2024-10-12 01:58:07 +0200

    Hero test / anim & TW position

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ I needed some time and look around to finally set up **Tailwindcss**, now it's u
 ![Alt Text](https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2lsdjFtYmRlb2oxbWkxdWVpNTdidGRzZXkzdmI3bXM5eXo2ajZvMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/k4kePT1XoOlvxlnglM/giphy.gif)
 
 I found out today that with **Tailwind**, if you want to write the long class names in a CSS file and @apply these, you have to assign them to the _html_ **element** and not the _className_. Using the most common _elements_ in web, it forces to think custom **tailwind** lines in a clear and organised way. Assigning them by _elements_ gives more concistency.
+
+10 minutes later :
+
+Forgot about className={styles.navbar}, done it just before ...
+
+![Alt Text](https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZnd3OXV1bm52OW1yeHhwaWx1Ynp0ZmZ0eWk5YXl2Z3cwNXZiaGE4aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YpmVBNubONoqs/giphy.gif)
+
+I start to see interest in **Tailwindcss** but it is still not my favorite way to write **css**.
+Can't find precision in the sizes and positions yet. p-96? w-72? h-1/3 ??
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,20 +3,30 @@ import "./components.module.css";
 
 const Hero = () => {
   return (
-    <main className="hero">
-      <div className="bg-lime-200 w-screen   h-96 relative mt-16 text-end flex flex-col justify-between m-auto p-9">
-        <motion.h1 className="text-orange-600 uppercase cursor-pointer text-8xl font-bold">
+    <main className="relative flex-col h-screen z-0 w-screen  bg-blue-100 bg-opacity-50 mt-36 justify-center align-middle m-auto">
+      <div className="w-screen h-96 relative  text-end flex flex-col justify-between  m-auto py-72 px-80">
+        <motion.h1
+          className="absolute right-96 top-24 text-orange-600 uppercase w-1/2 cursor-pointer text-8xl font-bold "
+          initial={{ opacity: 0, y: 200 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.3, ease: "backOut" }}>
           Front-End Developer
         </motion.h1>
-        <motion.h2 className="text-white uppercase text-7xl font-bold">
+        <motion.h2
+          className="absolute right-96 text-white w-1/2 uppercase text-7xl font-bold "
+          initial={{ opacity: 0, x: 500 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.3, ease: "backOut" }}>
           Visual Arts Enthusiast
         </motion.h2>
-        <motion.h3 className="text-orange-400 uppercase text-5xl font-bold;">
-          <motion.span>Currently Upskilling in Web</motion.span>
+        <motion.p className="text-orange-400 w-1/2 uppercase text-5xl font-bold;">
+          <motion.span className="font-bold px-8 ">
+            Currently Upskilling in Web
+          </motion.span>
           <motion.button className=" text-white font-bold;">
             @BeCode.org
           </motion.button>
-        </motion.h3>
+        </motion.p>
       </div>
     </main>
   );
